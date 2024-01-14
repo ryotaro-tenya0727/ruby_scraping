b3b5731ccdeab7c40b1652c3911d5a23cfbfa896
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2024-01-14 21:44:32 +0900

    Fix: 微修正

--- buy_auto_machine.rb
+++ buy_auto_machine.rb
@@ -15,7 +15,10 @@
 # -*- coding: utf-8 -*-
 require 'selenium-webdriver'
 require 'dotenv'
-driver = Selenium::WebDriver.for :chrome #ドライバ選択
+options = Selenium::WebDriver::Chrome::Options.new
+options.add_argument('--headless')
+options.add_argument('--window-size=1929,2160')
+driver = Selenium::WebDriver.for :chrome, options: options #ドライバ選択
 driver.get "https://t.livepocket.jp/login?acroot=header-new_p_u_nl"
 puts driver.title #タイトルを出力
 
@@ -30,7 +33,9 @@ pass = ENV['PASS']
 driver.find_element(:xpath, '//*[@id="email"]').send_key email
 driver.find_element(:xpath, '//*[@id="password"]').send_key pass
 driver.find_element(:xpath, '//*[@id="form"]/p[5]/button').click
+sleep 2
 driver.get "https://t.livepocket.jp/e/vs30min"
+puts "成功"
 sleep 500
 
 # driver.quit
