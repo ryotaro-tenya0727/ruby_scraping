3c7f5b0b23fb0cbdcf7cc79fcec96eb67e9b53e8
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2024-01-14 22:39:41 +0900

    Fix: 枚数入れるところまで

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 gem 'launchy', '~> 2.5', '>= 2.5.2'
 gem 'selenium-webdriver', '~> 4.16'
 gem "dotenv"
+gem 'pry-rails'
--- buy_auto_machine.rb
+++ buy_auto_machine.rb
@@ -15,9 +15,11 @@
 # -*- coding: utf-8 -*-
 require 'selenium-webdriver'
 require 'dotenv'
+require 'pry'
+
 options = Selenium::WebDriver::Chrome::Options.new
-options.add_argument('--headless')
-options.add_argument('--window-size=1929,2160')
+# options.add_argument('--headless')
+# options.add_argument('--window-size=1929,2160')
 driver = Selenium::WebDriver.for :chrome, options: options #ドライバ選択
 driver.get "https://t.livepocket.jp/login?acroot=header-new_p_u_nl"
 puts driver.title #タイトルを出力
@@ -33,9 +35,18 @@ pass = ENV['PASS']
 driver.find_element(:xpath, '//*[@id="email"]').send_key email
 driver.find_element(:xpath, '//*[@id="password"]').send_key pass
 driver.find_element(:xpath, '//*[@id="form"]/p[5]/button').click
-sleep 2
-driver.get "https://t.livepocket.jp/e/vs30min"
-puts "成功"
+sleep 3
+ticket_url = "https://t.livepocket.jp/e/idolkoushien-0223-"
+driver.get ticket_url
+select_elements = driver.find_elements(:css, 'select[id^="ticket-"]')
+min_select_element = select_elements.min_by { |element| element.attribute('id').split("-").last.to_i }
+# Selectクラスを使って要素を選択します
+select = Selenium::WebDriver::Support::Select.new(min_select_element)
+
+# インデックスで選択
+select.select_by(:index, 1)
+# binding.pry
+puts driver.title
 sleep 500
 
 # driver.quit
