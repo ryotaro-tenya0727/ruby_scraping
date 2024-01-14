571dd12f0cfe9f2aaa8978d50a9bb5460b169ce7
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2024-01-14 20:49:06 +0900

    Fix: 微修正

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem 'launchy', '~> 2.5', '>= 2.5.2'
+gem 'selenium-webdriver', '~> 4.16'
+gem "dotenv"
---
+++ buy_auto_machine.rb
@@ -0,0 +1,35 @@
+# require 'open-uri'
+# require 'nokogiri'
+# require 'launchy'
+
+# url = 'https://www.yahoo.co.jp/'
+
+# html = URI.open(url).read
+# Launchy.open(url)
+
+# doc = Nokogiri::HTML.parse(html)
+
+# title = doc.title
+# puts title
+
+# -*- coding: utf-8 -*-
+require 'selenium-webdriver'
+require 'dotenv'
+driver = Selenium::WebDriver.for :chrome #ドライバ選択
+driver.get "https://t.livepocket.jp/login?acroot=header-new_p_u_nl"
+puts driver.title #タイトルを出力
+
+# meta descriptionを取ってくる場合
+# element = driver.find_element(:name, 'description')
+# puts element.attribute('content')
+
+Dotenv.load
+email = ENV['EMAIL']
+pass = ENV['PASS']
+
+driver.find_element(:xpath, '//*[@id="email"]').send_key email
+driver.find_element(:xpath, '//*[@id="password"]').send_key pass
+driver.find_element(:xpath, '//*[@id="form"]/p[5]/button').click
+sleep 500
+
+# driver.quit
